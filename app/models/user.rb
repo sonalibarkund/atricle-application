ec122e83e8bb0ccfc765005e8484dc2c2bd89d9c
supriyabarkund <barkundsupriya28@gmail.com>
2017-08-19 15:52:34 +0530

    User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
 	has_many :comments
+	validates :username, presence: true, length: {minimum: 3, maximum: 20}, uniqueness:{case_sensitive: false}
+	VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	validates :email, presence: true, length: {maximum: 20},uniqueness:{case_sensitive: false}, 
+			   format: {with: VALID_EMAIL_REGEX}
 end
